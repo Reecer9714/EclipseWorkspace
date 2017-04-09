a0ce8a376faee72c1a05b1d757b114ef05a27ad5
Reece Burch <reecer97@yahoo.com>
2017-04-09 00:56:59 -0500

    text editor now saves to a loaded file
    the only way to load file is from file browser

--- Hacking/src/hacking/main/programs/gui/TextEditor.java
+++ Hacking/src/hacking/main/programs/gui/TextEditor.java
@@ -1,58 +1,73 @@
 package hacking.main.programs.gui;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-import hacking.main.GUIGame;
 import hacking.main.ReaperOS;
 import hacking.main.files.TextFile;
 
 public class TextEditor extends GUIProgram{
 
     private JTextArea textArea;
-    private ImageIcon icon;
     private TextFile currentFile;
 
-    public TextEditor(ReaperOS os, ImageIcon icon,  int width, int height){
+    public TextEditor(ReaperOS os, ImageIcon icon, int width, int height){
 	super(os, "TextEditor", icon, width, height);
 	JMenuBar menuBar_1 = new JMenuBar();
 	setJMenuBar(menuBar_1);
-	
+
 	JMenu mnFile = new JMenu("File");
 	menuBar_1.add(mnFile);
-	
-	JMenuItem mntmSave = new JMenuItem("Save");
-	mnFile.add(mntmSave);
-	
-	JMenuItem mntmLoad = new JMenuItem("Load");
-	mnFile.add(mntmLoad);
-	
+
+	JMenuItem saveOption = new JMenuItem("Save");
+	saveOption.addActionListener(new ActionListener(){
+	    @Override
+	    public void actionPerformed(ActionEvent e){
+		save();
+	    }
+	});
+	mnFile.add(saveOption);
+
+	JMenuItem loadOption = new JMenuItem("Load");
+	loadOption.addActionListener(new ActionListener(){
+	    @Override
+	    public void actionPerformed(ActionEvent e){
+		// TODO; add popup to select file
+		// load();
+	    }
+	});
+	mnFile.add(loadOption);
+
 	JScrollPane scrollPane = new JScrollPane();
 	getContentPane().add(scrollPane, BorderLayout.CENTER);
-	
+
 	textArea = new JTextArea();
 	scrollPane.setViewportView(textArea);
-	
-	icon = new ImageIcon(GUIGame.class.getResource("/texteditor.png"), "Desktop Icon");
+
     }
-    
+
     @Override
     public void close(){
+	super.close();
 	textArea.setText("");
 	currentFile = null;
     }
-    
+
     public void load(TextFile file){
-	textArea.setText(file.getContents());
+	textArea.setText(file.getBody());
 	currentFile = file;
     }
-    
+
     public void save(){
-	//TODO: create new TextFile and put in Folder
+	// TODO: create new TextFile and put in Folder
 	if(currentFile == null){
-	    //TODO: add popup to select where to save file and file name
-	    //currentFile = new TextFile(os, title, null);
+	    // TODO: add popup to select where to save file and file name
+	    // currentFile = new TextFile(os, title, null);
+	}else{
+	    currentFile.setBody(textArea.getText());
 	}
     }
 
