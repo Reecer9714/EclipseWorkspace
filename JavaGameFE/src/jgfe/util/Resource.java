d4710bf86547fabde74eb1849bf3680c94cf84aa
Reece Burch <reecer97@yahoo.com>
2018-04-13 16:41:26 -0500

    removed Old projects

--- JavaGameFE/src/jgfe/util/Resource.java
+++
@@ -1,7 +0,0 @@
-package jgfe.util;
-
-public abstract class Resource{
-    public String name;
-    
-    public abstract Resource load(String path);
-}
--- JavaGameFE/src/jgfe/util/ShadowType.java
+++
@@ -1,6 +0,0 @@
-package jgfe.util;
-
-public enum ShadowType {
-	NONE, TOTAL, FADE, HALF;
-	
-}
